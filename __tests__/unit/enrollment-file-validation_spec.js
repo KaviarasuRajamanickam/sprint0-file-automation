72455fe6595c20bbf7aa9d457a0209d4eced54d2
kaviarasu-hobsons <kaviarasu.rajamanickam@hobsons.com>
2020-08-27 14:01:43 +0530

    code review and jest reporter added

--- __tests__/unit/enrollment-file-validation_spec.js
+++ __tests__/unit/enrollment-file-validation_spec.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
 const fileValidator = require('../../controllers/file-validation')
+const positiveUserScenario = ['testData/users-input-file.csv', 'testData/users-output-file.csv']
+const positiveEnrollmentScenario = ['testData/enrollment-input-file.csv', 'testData/enrollment-output-file.csv']
 let userInfo, enrollmentInfo, inputRecords, outputRecords
 
 describe('To assert the datas present in the enrollment file', () => {
   beforeAll(async () => {
-    userInfo = await fileValidator.getAnonymizeDataWithIID('testData/users-input-file.csv', 'testData/users-output-file.csv')
-    enrollmentInfo = await fileValidator.dataInfo('testData/enrollment-input-file.csv', 'testData/enrollment-output-file.csv')
+    userInfo = await fileValidator.getAnonymizeDataWithIID(positiveUserScenario)
+    enrollmentInfo = await fileValidator.dataInfo(positiveEnrollmentScenario)
     inputRecords = enrollmentInfo.inputData.records
     outputRecords = enrollmentInfo.outputData.records
   })
@@ -73,5 +75,45 @@ describe('To assert the datas present in the enrollment file', () => {
       }
     })
     expect(parentIntegrationId).toBe(true)
+  })
+
+  /** Generic Validations **/
+  it('Verify whether the number of records are correctly matched between the input file and output file.', () => {
+    expect(outputRecords.length).toEqual(inputRecords.length)
+  })
+
+  it('Verify whether the Headers are correctly displayed in the output file.', () => {
+    expect(enrollmentInfo.inputData.header[0]).toEqual(enrollmentInfo.outputData.header[0])
+  })
+
+  it('Verify that the output file column with empty value is correctly shown based on the empty value in the input file.', () => {
+    let emptyColumnStatus = true
+    const dataFields = fileValidator.getEnrollmentInfo().non_anonymize_fields
+    inputRecords.forEach((currentRecord, index) => {
+      dataFields.forEach(currentField => {
+        try {
+          if (currentRecord[currentField] === '') {
+            expect(outputRecords[index][currentField]).toBe('')
+          }
+        } catch (err) {
+          emptyColumnStatus = false
+          console.error(currentField + ' - [' + currentRecord[currentField] + '] input value does not match with the output value [' + outputRecords[index][currentField] + ']')
+        }
+      })
+    })
+    expect(emptyColumnStatus).toBe(true)
+  })
+
+  it('Verify that the file column with date value is correctly shown based on the date value present in the input file.', () => {
+    let dateValueStatus = true
+    outputRecords.forEach(function (currentRecord, index) {
+      try {
+        currentRecord.last_access_date === '' ? expect(currentRecord.last_access_date).toBe('') : expect(fileValidator.verifyTimestamp(currentRecord.last_access_date)).toBeTruthy()
+      } catch (err) {
+        dateValueStatus = false
+        console.error('last_access_date - [' + currentRecord.last_access_date + '] output value does not displayed in the format yyyy-mm-dd hh:mm:ss [' + inputRecords[index].last_access_date + ']')
+      }
+    })
+    expect(dateValueStatus).toBe(true)
   })
 })
--- __tests__/unit/relationship-file-validation_spec.js
+++ __tests__/unit/relationship-file-validation_spec.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
 const fileValidator = require('../../controllers/file-validation')
+const positiveUserScenario = ['testData/users-input-file.csv', 'testData/users-output-file.csv']
+const positiveRelationshipScenario = ['testData/relationship-input-file.csv', 'testData/relationship-output-file.csv']
 let userInfo, relationshipInfo, inputRecords, outputRecords
 
 describe('To assert the datas present in the relationship file', () => {
   beforeAll(async () => {
-    userInfo = await fileValidator.getAnonymizeDataWithIID('testData/users-input-file.csv', 'testData/users-output-file.csv')
-    relationshipInfo = await fileValidator.dataInfo('testData/relationship-input-file.csv', 'testData/relationship-output-file.csv')
+    userInfo = await fileValidator.getAnonymizeDataWithIID(positiveUserScenario)
+    relationshipInfo = await fileValidator.dataInfo(positiveRelationshipScenario)
     inputRecords = relationshipInfo.inputData.records
     outputRecords = relationshipInfo.outputData.records
   })
@@ -30,7 +32,7 @@ describe('To assert the datas present in the relationship file', () => {
   it('Verify whether the User_Integration_Id value is not anonymized in the output file, when the user_integration_id column has some random value on the input Relationship.txt file', () => {
     let parentIntegrationIdStatus = true
     inputRecords.forEach(function (currentRecord, index) {
-      var checkRecord = userInfo.userIntegrationData.filter(record => (record.original_integration_id === currentRecord.parent_integration_id))
+      const checkRecord = userInfo.userIntegrationData.filter(record => (record.original_integration_id === currentRecord.parent_integration_id))
       if (!checkRecord.length) {
         try {
           expect(outputRecords[index].parent_integration_id).toBe(currentRecord.parent_integration_id)
@@ -109,38 +111,7 @@ describe('To assert the datas present in the relationship file', () => {
   })
 
   /** Generic Validations **/
-  it('Verify whether the actual records from input file are correctly spooled in the output File', () => {
-    const dataFeilds = fileValidator.getRelationshipInfo().anonymize_fields.concat(fileValidator.getRelationshipInfo().non_anonymize_fields)
-    expect(relationshipInfo.outputData.header[0]).toEqual(relationshipInfo.inputData.header[0])
-    outputRecords.forEach(function (currentRecord, index) {
-      dataFeilds.forEach(function (currentField) {
-        if (currentRecord[currentField] === '') {
-          expect(currentRecord[currentField]).toBe('')
-        } else if (currentField === 'parent_integration_id') {
-          inputRecords.forEach(function (currentRecord, index) {
-            userInfo.userIntegrationData.forEach(function (currentField) {
-              if (currentRecord.parent_integration_id === currentField.original_integration_id) {
-                expect(outputRecords[index].parent_integration_id).toBe(currentField.masked_integration_id)
-              }
-            })
-          })
-        } else if (currentField === 'child_id') {
-          inputRecords.forEach(function (currentRecord, index) {
-            userInfo.userIntegrationData.forEach(function (currentField) {
-              if (currentRecord.child_id === currentField.original_integration_id) {
-                expect(outputRecords[index].child_id).toBe(currentField.masked_integration_id)
-              }
-            })
-          })
-        } else {
-          expect(currentRecord[currentField]).toBe(inputRecords[index][currentField])
-        }
-      })
-    })
-  })
-
   it('Verify whether the number of records are correctly matched between the input file and output file.', () => {
-    expect(relationshipInfo.outputData.header[0].length).toEqual(relationshipInfo.inputData.header[0].length)
     expect(outputRecords.length).toEqual(inputRecords.length)
   })
 
@@ -149,22 +120,38 @@ describe('To assert the datas present in the relationship file', () => {
   })
 
   it('Verify that the output file column with empty value is correctly shown based on the empty value in the input file.', () => {
-    const userFields = fileValidator.getRelationshipInfo().non_anonymize_fields
-    userFields.forEach(function (currentRecord) {
-      if (inputRecords.currentRecord === '') {
-        expect(inputRecords.currentRecord).toBe('')
-      }
+    let emptyColumnStatus = true
+    const dataFields = fileValidator.getRelationshipInfo().non_anonymize_fields
+    inputRecords.forEach((currentRecord, index) => {
+      dataFields.forEach(currentField => {
+        try {
+          if (currentRecord[currentField] === '') {
+            expect(outputRecords[index][currentField]).toBe('')
+          }
+        } catch (err) {
+          emptyColumnStatus = false
+          console.error(currentField + ' - [' + currentRecord[currentField] + '] input value does not match with the output value [' + outputRecords[index][currentField] + ']')
+        }
+      })
     })
+    expect(emptyColumnStatus).toBe(true)
   })
 
   it('Verify that the file column with numeric value is correctly shown based on the numeric value present in the input file.', () => {
+    let numericValStatus = true
     const dataFeilds = fileValidator.getRelationshipInfo().non_anonymize_fields
     outputRecords.forEach(function (currentRecord, index) {
       dataFeilds.forEach(function (currentField) {
-        currentRecord[currentField] === ''
-          ? expect(currentRecord[currentField]).toBe('')
-          : expect(currentRecord[currentField]).toBe(outputRecords[index][currentField])
+        try {
+          currentRecord[currentField] === ''
+            ? expect(currentRecord[currentField]).toBe('')
+            : expect(currentRecord[currentField]).toBe(outputRecords[index][currentField])
+        } catch (err) {
+          numericValStatus = false
+          console.error(dataFields[index] + ' - [' + inputRecords[outputIndex][currentField] + '] input numeric value does not match with the output numeric value [' + currentRecord[currentField] + ']')
+        }
       })
     })
+    expect(numericValStatus).toBe(true)
   })
 })
